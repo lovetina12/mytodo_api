13223c63f1bb9e89cfd8277afd7861b40566b2e2
lovetina12 <aprosper2580@gmail.com>
2021-12-14 11:09:34 +0000

    a todo api in my project

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const res = require('express/lib/response');
+require('dotenv/config');
+const bodyParser = require('body-parser');
+const app = express();
+const todoModel = require('./models/todo_model.js');
+app.use(bodyParser.json());
+
+app.get('/',(req, res)=>{
+    res.send('this is our second api we are building');
+});
+app.post('/ todo', async(req, res)=>{
+    const todo = todoModel.create({
+        title:req.body.title,
+        body:req.body.body,
+        status:req.body.status,
+        endDate:req.body.endDate
+    });
+    try{
+        const saveTodo = await todo.save();
+            res.json({
+            dataTodo,
+            message:"todo successfully created"
+        });
+    }catch(err){
+        res.json({
+            mressage:err
+        });
+    }
+});
+app.get('/todo',async(req, res)=>{
+    try{
+       const getAllTodos= await todoModel.find();
+       res.json({
+        data:getAllTodos,
+        message:"operation sucessful" });
+    }catch(err){
+        res.json({
+            message:err
+        });
+       
+    }
+});
+app.get('/todo/:todoid',async(req, res)=>{
+    try{
+       const getAllTodos= await todoModel.findById({_id:req.body.todoid});
+       res.json({
+        data:getAllTodos,
+        message:"operation sucessful" });
+    }catch(err){
+        res.json({
+            message:err
+        });
+       
+    }
+});
+app.delete('/todos/todoId', async(req, res)=>{
+    try{
+    const deleteTodo = await todoModel.findOneAndDelete({_id:req.params.todoId});
+    res.json({
+        data:deleteTodo,
+        message:"Todo successfully delete"
+    });
+    }catch(err){
+        res.json({
+            message:err
+        });
+    }    
+});
+
+app.patch('/todo/:todoId',(req, res)=>{
+    todoModel.findOneAndUpdate({_id:req.params.todoid,},{$set:{
+        title: req.body,title,
+        status:req.body.status,
+        body:req.body.body
+    }});
+})
+
+
+mongoose.connect(process.env.DB_URL, 
+()=>console.log('successfully connected'));
+
+app.listen(1020);
---
+++ models/todo_model.js
@@ -0,0 +1,23 @@
+const { status } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        default: false
+    },
+    endDate:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('todoModel',todoSchema);
